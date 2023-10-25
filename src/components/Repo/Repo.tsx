6072922c776775eaa408a2b680ff812eec4bc0a1
azizoid <shaqhuseynov@gmail.com>
2023-10-25 03:09:09 +0200

    feat: diplay buttons as buttons on hover, othervise apply text style

--- src/components/Repo/Repo.tsx
+++ src/components/Repo/Repo.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { Button } from '../Button/Button';
 
 export interface RepositoryProps {
@@ -9,12 +11,21 @@ export interface RepositoryProps {
   subscribersCount:number
 }
 
-export const Repo = ({fullname, description, url, stars, subscribersCount}: RepositoryProps) =>
-  <div className="p-4 bg-white rounded shadow-lg">
-    <p className="text-gray-600 font-bold">{description}</p>
-    <p className="text-blue-500 underline mb-2"><a href={url} target="_blank">{fullname}</a></p>
-    <div className="flex justify-start gap-1">
-      <Button>{stars} ulduz</Button>
-      <Button>dəstək ol ({subscribersCount})</Button>
+export const Repo = ({fullname, description, url, stars, subscribersCount}: RepositoryProps) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <div
+      className="p-4 bg-white rounded shadow-lg"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <p className="text-gray-600 font-bold">{description}</p>
+      <p className="text-blue-500 underline my-2"><a href={url} target="_blank">{fullname}</a></p>
+      <div className="flex justify-start gap-1">
+        <Button type={isHovered ? 'button' : 'text'}>{stars} ulduz</Button>
+        <Button type={isHovered ? 'button' : 'text'}>dəstək ol ({subscribersCount})</Button>
+      </div>
     </div>
-  </div>
+  )
+}
