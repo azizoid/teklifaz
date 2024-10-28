a1a537d56ec25f30b0e5dce1a40aedcb7dd9347b
azizoid <shaqhuseynov@gmail.com>
2024-10-28 19:31:55 +0100

    style: set header and footer styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,22 +1,22 @@
 import Link from "next/link";
 
 export const Header = () => (
-  <header className="bg-teklif-700 text-white border-gray-300 px-3 py-1">
-    <nav className="container mx-auto p-4 flex justify-between items-center">
-      <h1 className="text-2xl font-bold"><Link href={'/'}>Teklif.az</Link></h1>
+  <header className="bg-white shadow-sm p-4">
+    <nav className="container mx-auto flex justify-between items-center">
+      <h1 className="text-xl font-semibold text-teklif-600"><Link href={'/'}>Teklif.az</Link></h1>
       <ul className="flex space-x-6">
         <li>
-          <Link href="/projects" className="hover:text-gray-300">
+          <Link href="/projects" className="text-gray-700 hover:text-teklif-600">
             Projects
           </Link>
         </li>
         <li>
-          <a href="#" className="hover:text-gray-300">
+          <a href="#" className="text-gray-700 hover:text-teklif-600">
             About
           </a>
         </li>
         <li>
-          <a href="#" className="hover:text-gray-300">
+          <a href="#" className="text-gray-700 hover:text-teklif-600">
             Contact
           </a>
         </li>
