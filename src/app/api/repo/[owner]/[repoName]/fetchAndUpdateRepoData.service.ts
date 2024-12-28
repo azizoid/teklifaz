37751490b73c90a829b9c0cbc302bd47ac809e4a
azizoid <shaqhuseynov@gmail.com>
2024-12-28 04:50:13 +0100

    feat: router and service file to fetch repo and update data in prisma

---
+++ src/app/api/repo/[owner]/[repoName]/fetchAndUpdateRepoData.service.ts
@@ -0,0 +1,37 @@
+import { prisma } from "@/lib/prismadb";
+import { CustomError } from "@/lib/CustomError";
+import { fetchGitHubRepo } from "@/utils/fetchGithubRepo";
+
+export const fetchAndUpdateRepoData = async (name: string, owner: string, repoName: string) => {
+  const repo = await prisma.repository.findUnique({ where: { name } });
+
+  try {
+    const githubResponse = await fetchGitHubRepo(owner, repoName, repo?.etag);
+
+    const conditionsData = {
+      stars: githubResponse.data.stargazers_count || 0,
+      activity: 0, // Placeholder for future logic
+      etag: githubResponse.headers.etag || null,
+      details: JSON.stringify(githubResponse.data),
+      lastUpdated: new Date(),
+    };
+
+    if (repo) {
+      return prisma.repository.update({ where: { name }, data: conditionsData });
+    } else {
+      return prisma.repository.create({
+        data: { name, repo_name: repoName, owner, ...conditionsData },
+      });
+    }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    if (error.status === 304) {
+      console.log('304 Not Modified. Returning existing repository from the database.');
+      if (repo) {
+        return repo;
+      }
+      throw new CustomError('Weird! No changes detected, but repository not found in the database. ', 500);
+    }
+    throw error;
+  }
+}
---
+++ src/app/api/repo/[owner]/[repoName]/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { isRepoExists } from '@/utils/isRepoExists'
+import { CustomError } from '@/lib/CustomError';
+import { fetchAndUpdateRepoData } from './fetchAndUpdateRepoData.service';
+import { handleError } from '@/utils/handleError';
+
+type Params = Promise<{ owner: string, repoName: string }>
+
+export const GET = async (_: NextRequest, res: { params: Params }) => {
+  try {
+    const { owner, repoName } = await res.params;
+
+    const id = `${owner}/${repoName}`;
+
+    // Biz, ancaq bizim icmanin bir hissesi olaran repo-lari yoxlayiriq
+    if (!isRepoExists(id)) {
+      throw new CustomError(
+        'This repository is not part of our community. To add this repo to the database, please follow the link.',
+        400
+      );
+    }
+
+    const repo = await fetchAndUpdateRepoData(id, owner, repoName);
+
+    return NextResponse.json(repo, { status: 200 });
+
+  } catch (error) {
+    return handleError(error);
+  }
+};
