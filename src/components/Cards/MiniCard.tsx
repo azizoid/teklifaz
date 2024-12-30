8449940ad5892a716537b8fa678ac8ce48aa7e9c
azizoid <shaqhuseynov@gmail.com>
2024-12-30 05:22:22 +0100

    feat: update ProjectsPage to fetch repositories from database; refactor card components to use new repo structure

--- src/components/Cards/MiniCard.tsx
+++ src/components/Cards/MiniCard.tsx
@@ -1,19 +1,19 @@
 import Link from "next/link";
-import { RepoCardProps } from "./RepoCard.type";
+import { RepoCardProps } from "./Card.types";
 import { CiStar } from "react-icons/ci";
 
-export const MiniCard = ({ project }: RepoCardProps) => (
+export const MiniCard = ({ repo }: RepoCardProps) => (
   <Link
-    href={`/projects/${project.developer}/${project.name}`}
+    href={`/projects/${repo.name}`}
     className="border-r border-b hover:bg-gray-50"
   >
     <div className="flex flex-row justify-between gap-2 p-4">
       <h5 className="text-gray-900">
-        {project.developer}/<span className="font-bold">{project.name}</span>
+        {repo.owner}/<span className="font-bold">{repo.repo_name}</span>
       </h5>
       <div className="flex items-center space-x-1">
         <CiStar size="16" />
-        <span className="">{Math.round(Math.random() * 100)}</span>
+        <span className="">{repo.stars}</span>
       </div>
     </div>
   </Link>
--- src/components/Cards/RepoCard.type.ts
+++
@@ -1,5 +0,0 @@
-import { ProjectProps } from "@/app/projects/getProjects";
-
-export type RepoCardProps = {
-  project: ProjectProps;
-};
